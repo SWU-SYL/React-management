1466ad74fe4d3aea51e011ee96d2317274389551
SWU-SYL <rezzo203@naver.com>
2021-11-22 01:28:58 +0900

    Create React Project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# SPEC-A 블록체인 인증서 개인정보 등록 소스코드
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="gray-background">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SPEC-A 블록체인 인증서 개인정보 등록
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="join up"
+          href="https://reactjs.org" //나중에 회원가입 페이지로 연동
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          회원가입
         </a>
       </header>
     </div>
